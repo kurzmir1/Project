79efbb1e87c9830e5b064e011dedab5538249b96
kurzmir1 <mirjam.kurz@hotmail.com>
2022-05-20 15:27:18 +0200

    made different rolling windows for speed and tried visualizing etc.

--- Project_Rscript.R
+++ Project_Rscript.R
@@ -15,36 +15,111 @@ library(ggplot2)      # to visualize data
 library(sf)           # to handle spatial vector data
 library(terra)        # To handle raster data
 library(lubridate)    # To handle dates and times
+library(zoo)
+
+load(file='wildschwein_BE.rda')
 
 wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
+
+##Calculate steplength
 
 wildschwein_BE <- wildschwein_BE %>%
   group_by(TierID) %>%
   mutate(steplength = sqrt((E- lead(E,1))^2 + (N -lead(N,1))^2))
 
+##Calculate timelag
+wildschwein_BE<-wildschwein_BE %>%
+  group_by(TierID) %>%
+  mutate(timelag=as.integer(difftime(lead(DatetimeUTC), DatetimeUTC,units="secs")))
+
+
+##Calculate speed
 wildschwein_BE <- wildschwein_BE %>%
   mutate(speed=steplength/timelag)
 
-wildschwein_BE <- wildschwein_BE %>%
-  mutate(
-    nMinus3 = sqrt((lag(E,3)-E)^2+(lag(N,3)-N)^2),   # distance to pos -3 minutes
-    nMinus2 = sqrt((lag(E,2)-E)^2+(lag(N,2)-N)^2),   # distance to pos -2 minutes
-    nMinus1 = sqrt((lag(E,1)-E)^2+(lag(N,1)-N)^2),   # distance to pos -1 minutes
-    nPlus1  = sqrt((E-lead(E,1))^2+(N-lead(N,1))^2), # distance to pos +1 mintues
-    nPlus2  = sqrt((E-lead(E,2))^2+(N-lead(N,2))^2),  # distance to pos +2 minutes
-    nPlus3  = sqrt((E-lead(E,3))^2+(N-lead(N,3))^2)  # distance to pos +3 minutes
-  )
+##Calculate mean speed for different rolling windows
+wildschwein_BE_win <- wildschwein_BE
+wildschwein_BE_win$k4 <- rollmean(wildschwein_BE_win$speed, k=4, fill = NA, align = "left")
+wildschwein_BE_win$k8 <- rollmean(wildschwein_BE_win$speed, k=8, fill = NA, align = "left")
+wildschwein_BE_win$k12 <- rollmean(wildschwein_BE_win$speed, k=12, fill = NA, align = "left")
+wildschwein_BE_win$k16 <- rollmean(wildschwein_BE_win$speed, k=16, fill = NA, align = "left")
+wildschwein_BE_win$k20 <- rollmean(wildschwein_BE_win$speed, k=20, fill = NA, align = "left")
 
-wildschwein_BE <- wildschwein_BE %>%
-  rowwise() %>%
-  mutate(
-    stepMean = mean(c(nMinus3, nMinus2, nMinus1,nPlus1,nPlus2,nPlus3))
-  ) %>%
-  ungroup() 
+##k4 values below 0.1 = static
 
-ggplot(data=wildschwein_BE, mapping = aes(x=speed))+
-  geom_histogram(binwidth = 1)+
-  xlim(0,500)+
-  ylim(0,1000)
+wildschwein_BE_win <- wildschwein_BE_win %>% 
+  ungroup() %>%
+  mutate(static = k4 < 0.1)
 
-min(wildschwein_BE$stepMean, na.rm = TRUE)
+##Filter for K4 static stretches
+wildschwein_BE_static <- wildschwein_BE_win %>%
+  filter(static)
+
+
+##Visualize static stretches (k4) per animal
+ggplot(data = wildschwein_BE_win, mapping = aes(x= E, y=N) )+
+  geom_path()+
+  geom_point(aes(colour=static))+
+  coord_equal()+
+  facet_wrap(~ TierName, ncol = 5)
+
+##K20 values below 0.1 = static
+wildschwein_BE_win <- wildschwein_BE_win %>% 
+  ungroup() %>%
+  mutate(staticK20 = k20 < 0.1)
+
+##Filter for K20 static stretches
+wildschwein_BE_staticK20 <- wildschwein_BE_win %>%
+  filter(staticK20)
+
+
+##Visualize filtered K20 static stretches
+ggplot(data = wildschwein_BE_staticK20, mapping = aes(x= E, y=N) )+
+  geom_path()+
+  geom_point(aes(colour=staticK20))+
+  coord_equal()+
+  facet_wrap(~ TierName, ncol = 5)
+
+##K4 values below 0.01
+wildschwein_BE_win <- wildschwein_BE_win %>% 
+  ungroup() %>%
+  mutate(verystatic = k4 < 0.01)
+
+
+##K8 values below 0.01
+wildschwein_BE_win <- wildschwein_BE_win %>% 
+  ungroup() %>%
+  mutate(verystaticK8 = k8 < 0.01)
+
+##Visualize very static stretches (k8) per animal
+ggplot(data = wildschwein_BE_win, mapping = aes(x= E, y=N) )+
+  geom_path()+
+  geom_point(aes(colour=verystaticK8))+
+  coord_equal()+
+  facet_wrap(~ TierName, ncol = 5)
+
+##Filter for K8 very static stretches
+wildschwein_BE_verystaticK8 <- wildschwein_BE_win %>%
+  filter(verystaticK8)
+
+##visualize filtered very static K8 stretches
+ggplot(data = wildschwein_BE_staticK20, mapping = aes(x= E, y=N) )+
+  geom_path()+
+  geom_point(aes(colour=staticK20))+
+  coord_equal()+
+  facet_wrap(~ TierName, ncol = 5)
+
+##Filter for K8 very static stretches of Ueli
+Ueli_verystaticK8 <- wildschwein_BE_verystaticK8 %>%
+  filter(TierName == "Ueli")
+
+##FInding the most static points of Ueli (0.001)
+Ueli_verystaticK8<- Ueli_verystaticK8 %>% 
+  ungroup() %>%
+  mutate(moststaticK8 = k8 < 0.001)
+
+##visualize most static points of Ueli
+ggplot(data = Ueli_verystaticK8, mapping = aes(x= E, y=N) )+
+  geom_path()+
+  geom_point(aes(colour=moststaticK8))+
+  coord_equal()
